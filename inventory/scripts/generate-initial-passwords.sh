d5db61a2ca96bb9dd9f32b2ddb0251c7f68c9cd1
PC-Admin <pc-admin@gitlab.com>
2020-10-16 13:59:04 +0800

    tweak initially generated variables

--- inventory/scripts/generate-initial-passwords.sh
+++ inventory/scripts/generate-initial-passwords.sh
@@ -23,6 +23,12 @@ matrix_synapse_macaroon_secret_key: $(generatePassword)
 # Element Settings
 matrix_client_element_enabled: true
 matrix_client_element_jitsi_preferredDomain: jitsi.$DOMAIN
+# Element Extension
+matrix_client_element_configuration_extension_json: |
+  {
+  "disable_3pid_login": true
+  }
+# End Element Extension
 # Jitsi Settings
 matrix_jitsi_enabled: true
 matrix_jitsi_jicofo_component_secret: $(generatePassword)
@@ -31,8 +37,18 @@ matrix_jitsi_jvb_auth_password: $(generatePassword)
 matrix_jitsi_jibri_recorder_password: $(generatePassword)
 matrix_jitsi_jibri_xmpp_password: $(generatePassword)
 # Synapse Settings
+matrix_synapse_container_metrics_api_host_bind_port: '9000'
+matrix_synapse_metrics_enabled: true
+matrix_synapse_metrics_port: 9100
 matrix_synapse_allow_public_rooms_over_federation: true
 matrix_synapse_enable_registration: false
+# Synapse Extension
+matrix_synapse_configuration_extension_yaml: |
+  admin_contact: 'mailto:chatoasis@protonmail.com'
+  mau_stats_only: true
+# End Synapse Extension
+# PostgreSQL Settings
+matrix_postgres_connection_password: $(generatePassword)
 # Base Domain Settings
 matrix_nginx_proxy_base_domain_homepage_enabled: false
 # Extra Settings
